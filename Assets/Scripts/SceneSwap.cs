a3feacc9dc04052cc98502ece7d940c5a8cb5f97
cpeng87 <alcp6201@gmail.com>
2026-01-15 21:34:39 -0500

    add scene swap and blank kitchen scene

---
+++ Assets/Scripts/SceneSwap.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneSwap : MonoBehaviour
+{
+    [SerializeField] private string nextScene;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the collision is with the player object
+        if (other.CompareTag("Player"))
+        {
+            SwapScene();
+        }
+    }
+
+    public void SwapScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
